9f34955a8a438750fde95e10ed4e0309574db40f
Samkit-Kothari-thg <Samkit.Kothari71@thg.com>
2024-07-28 15:06:41 +0530

    Updated styling and added photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import './App.scss';
-import PersonalInfo from './components/PersonalInfo';
-import FamilyInfo from './components/FamilyInfo';
-import ContactInfo from './components/ContactInfo';
-import Hobbies from './components/Hobbies';
+import React from "react";
+import "./App.scss";
+import PersonalInfo from "./components/PersonalInfo";
+import FamilyInfo from "./components/FamilyInfo";
+import ContactInfo from "./components/ContactInfo";
+import Hobbies from "./components/Hobbies";
+import PhotoFrame from "./components/PhotoFrame";
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Yogesh Bothara</h1>
       </header>
       <main>
+        <PhotoFrame />
         <PersonalInfo />
         <FamilyInfo />
         <ContactInfo />
---
+++ src/components/PhotoFrame/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './PhotoFrame.module.scss';
+import image1 from "../../assets/img-1.jpeg"
+import image2 from "../../assets/img-2.jpeg"
+const PhotoFrame: React.FC = () => {
+  return (
+    <div className={styles.photoFrameContainer}>
+      <div className={styles.photoFrame}>
+        <img src={image1} alt="Yogesh Dipakkumar Bothara" />
+      </div>
+      <div className={styles.photoFrame}>
+        <img src={image2} alt="Yogesh Dipakkumar Bothara" />
+      </div>
+    </div>
+  );
+};
+
+export default PhotoFrame;
