99f6ed76614182c585fd561f369455b51387af40
Samkit-Kothari-thg <Samkit.Kothari71@thg.com>
2024-07-28 18:28:42 +0530

    Links Added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import PersonalInfo from "./components/PersonalInfo";
 import FamilyInfo from "./components/FamilyInfo";
 import ContactInfo from "./components/ContactInfo";
 import PhotoFrame from "./components/PhotoFrame";
-
+import phoneIcon from "./assets/phone.png";
+import emailIcon from "./assets/email.png";
+import linkedInIcon from "./assets/linkedin.png";
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -17,7 +19,18 @@ const App: React.FC = () => {
         <FamilyInfo />
         <ContactInfo />
       </main>
-      <footer className="App-header">
+      <footer className="App-Footer">
+        <div className="links">
+          <a href="tel:8275057303">
+            <img className="logo" src={phoneIcon} alt="Phone" />
+          </a>
+          <a href="mailto:botharayogesh@gmail.com">
+            <img className="logo" src={emailIcon} alt="Email" />
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/yogesh-bothara-25476018b">
+            <img className="logo" src={linkedInIcon} alt="LinkedIn" />
+          </a>
+        </div>
         <span>&#169; of website by Samkit Kothari</span>
       </footer>
     </div>
